e0aa469cb80f0ddd3060c4ceddb558869c938886
Nirbhay-Tak <nirbhaytak2006@gmail.com>
2025-06-28 10:31:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+ const navbar = document.getElementById('navbar');
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 50) {
+                navbar.classList.add('nav-scrolled');
+            } else {
+                navbar.classList.remove('nav-scrolled');
+            }
+        });
+        // Smooth scroll for anchor links
+        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function(e) {
+                e.preventDefault();
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
+        });
